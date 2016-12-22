e77eca7c2b9048a662932b1354de9f598d1364f7
Marko Cupic <m.cupic@gmx.ch>
2016-12-22 21:56:07 +0100

    dev

---
+++ src/Resources/contao/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'ce_fixed_tabs_aside_item' => 'vendor/markocupic/fixed-tabs-aside-bundle/src/Resources/contao/templates',
+	'j_fixed_tabs_aside'       => 'vendor/markocupic/fixed-tabs-aside-bundle/src/Resources/contao/templates',
+));
